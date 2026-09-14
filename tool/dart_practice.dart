0e788a9c3149dd8a587a7c3981ba1522ddcc2e3a
H <161506051+Hahaverse@users.noreply.github.com>
2026-09-14 23:03:41 +0900

    feat: MISSION 2 해결

---
+++ tool/dart_practice.dart
@@ -0,0 +1,34 @@
+class Movie{
+    
+    const Movie({
+        required this.id,
+        required this.title,
+    });
+    final int id;
+    final String title;
+}
+
+String displayName(String? nickname) {
+  return nickname?.trim()
+  .isNotEmpty == true ? nickname! : '이름 없음';
+}
+
+final movie_list = <Movie>[
+    const Movie(id: 1, title: '곡성'),
+    const Movie(id: 2, title: '곤지암'),
+    const Movie(id: 3, title: '주온'),
+];
+
+void main(){
+    for (final movie in movie_list){
+        print(movie.title);
+    }
+
+    print(displayName(null));
+    print(displayName(''));
+    print(displayName('  '));
+    print(displayName('  무비러버  '));
+}
+
+
+
